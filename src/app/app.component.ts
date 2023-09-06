d625a63ab9a3b2c177ca4a04ec2c9685e80096d8
Kamil Gałek <kamil.galek@appfire.com>
2023-09-06 08:10:17 +0200

    application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import mondaySdk from 'monday-sdk-js';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <button (click)="click1()">Click 1</button>
+    <button (click)="click2()">Click 2</button>
+  `,
 })
 export class AppComponent {
-  title = 'monday-refresh-problem';
+
+  monday: any = mondaySdk();
+
+  click1(): void {
+    // this.monday.set('location', { query: { appPath: 'box/ROOT/o/hierarchy' } });
+    this.monday.set('location', { query: { appPath: 'box-ROOT-o-hierarchy' } });
+  }
+
+  click2(): void {
+    // this.monday.set('location', { query: { appPath: 'box/ROOT/o/timeline' } });
+    this.monday.set('location', { query: { appPath: 'box-ROOT-o-timeline' } });
+  }
+
 }
